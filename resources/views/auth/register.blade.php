ab75c9717df40662c4d5a290b754b311a220f16e
bestmomo <colorstonic@free.fr>
2015-04-17 01:30:10 +0200

    Register view bug

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,12 +7,12 @@
                     <div class="row">
 
                         <div class="form-group col-lg-6 {{ $errors->has('name')? 'has-error' : '' }}">
-                            <input class="form-control" placeholder="Name" name="name" type="text" value="{{ old('name') }} required">
+                            <input class="form-control" placeholder="Name" name="name" type="text" value="{{ old('name') }}" required>
                             {!! $errors->first('name', '<small class="help-block">:message</small>') !!}
                         </div>
                         
                         <div class="form-group col-lg-6 {{ $errors->has('email')? 'has-error' : '' }}">
-                            <input class="form-control" placeholder="E-Mail Address" name="email" type="email" value="{{ old('email') }} required">
+                            <input class="form-control" placeholder="E-Mail Address" name="email" type="email" value="{{ old('email') }}" required>
                             {!! $errors->first('email', '<small class="help-block">:message</small>') !!}
                         </div>
                         
